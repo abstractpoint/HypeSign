5068484d2aa833ea3dd3e0635a25aa21cc2f1361
George <george@abstractpoint.co.uk>
2015-10-21 21:46:29 +0100

    Colour to red

--- src/sign.js
+++ src/sign.js
@@ -1,8 +1,8 @@
 var net = require("net");
 module.exports = function(host, port) {
     var start = new Buffer("9901010147001B5B324A", "hex");
-    var line1 = new Buffer("1B5B313B31481B5B33333B34306D1B2335", "hex");
-    var line2 = new Buffer("1B5B323B31481B5B33333B34306D1B2335", "hex");
+    var line1 = new Buffer("1B5B313B31481B5B33313B34306D1B2335", "hex");
+    var line2 = new Buffer("1B5B323B31481B5B33313B34306D1B2335", "hex");
     var end = new Buffer("FF", "hex");
     return {
         send: function(l1, l2) {
