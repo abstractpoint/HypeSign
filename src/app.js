d8ad184ac0745733dfefa2a085fa77813e8bfb36
George <george@abstractpoint.co.uk>
2015-10-21 21:06:07 +0100

    time mod

--- src/app.js
+++ src/app.js
@@ -1,17 +1,40 @@
-// Twitter
-var Twitter = require('node-tweet-stream');
-var t = new Twitter(require('./credentials.json'));
+
+//var Twitter = require('node-tweet-stream');
+//var t = new Twitter(require('./credentials.json'));
 
 var sign = require('./sign')('10.0.0.201', 3500);
 
 var extractor = require('./extractor');
 
-t.on('tweet', function (tweet) {
-    console.log('tweet recieved', tweet);
-    var lines = extractor.extract(tweet.text);
-    if (! lines) return;
 
-    sign.send(lines.line1,lines.line2);
-});
+var exec = require('child_process').exec;
 
-t.follow('3874743447');
+
+function sendTime() {
+
+	exec('date', function(error, stdout, stderr){
+	        console.log(stdout);
+	        string = stdout;
+
+	        var lines = extractor.extract("@test " + string);
+	        if (! lines) return;
+	        sign.send(lines.line1,lines.line2);
+
+	});
+
+}
+
+
+setTimeout(function(){
+	sendTime();
+}, 1000);
+
+// t.on('tweet', function (tweet) {
+//     console.log('tweet recieved', tweet);
+//     var lines = extractor.extract(tweet.text);
+//     if (! lines) return;
+
+//     sign.send(lines.line1,lines.line2);
+// });
+
+// t.follow('3874743447');
